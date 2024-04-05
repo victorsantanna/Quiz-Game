79ac56f42009bdb089d7a1f01764d2d101691273
victorsantanna <victor.esef@hotmail.com>
2024-04-05 08:00:36 -0300

    feat: adição da funcionalidade de trocar as perguntas

--- src/App.vue
+++ src/App.vue
@@ -17,13 +17,17 @@
       <button v-if="!this.answerSubmitted" @click="this.submitAnswer()" class="send" type="button">Send</button>
       
       <section v-if="this.answerSubmitted" class="result">
-        <h4 v-if="this.chosenAnswer == this.correctAnswer">
-          &#9989;Congratulations, the answer "{{ this.correctAnswer }}" is corrext. 
+        <h4 v-if="this.chosenAnswer == this.correctAnswer"
+        v-html=" '&#9989;Congratulations, the answer ' + this.correctAnswer +  ' is corrext.' "
+        >
         </h4>
 
-        <h4 v-else>
-          &#10060;I'm Sorry, You picked the Wrong answer. The Correct is "{{ this.correctAnswer }}".
+        <h4 v-else
+        v-html=" '&#10060; Im Sorry, You picked the Wrong answer. The Correct is ' + this.correctAnswer + '.'"
+        >
+         
         </h4>
+        <button @click="this.getNewQuestion" class="send" type="button">Next question</button>
       </section>
 
     </template>
@@ -68,19 +72,29 @@ export default {
           console.log('Você Errou');
         }
       }
+    },
+    getNewQuestion(){
+
+      this.answerSubmitted = false;
+      this.chosenAnswer = undefined;
+      this.question = undefined;
+
+      this.axios
+      .get('https://opentdb.com/api.php?amount=1&category=18&difficulty=easy')
+      .then((response) =>{
+        this.question = response.data.results[0].question;
+        this.incorrectAnswers = response.data.results[0].incorrect_answers;
+        this.correctAnswer = response.data.results[0].correct_answer;
+      });
     }
+
+
   },
 
 
 
   created(){
-    this.axios
-    .get('https://opentdb.com/api.php?amount=1&category=18&difficulty=easy')
-    .then((response) =>{
-      this.question = response.data.results[0].question;
-      this.incorrectAnswers = response.data.results[0].incorrect_answers;
-      this.correctAnswer = response.data.results[0].correct_answer;
-    })
+   this.getNewQuestion();
   }
  
 }
