d14a8ba566cfc9a38e3a56d1103a2613099bc697
victorsantanna <victor.esef@hotmail.com>
2024-04-05 09:48:41 -0300

    feat: criação do componente ScoreBoard

---
+++ src/components/ScoreBoard.vue
@@ -0,0 +1,29 @@
+<template>
+
+    <section class="score">
+        Player <span>{{ this.winCount }}</span> X <span>{{ this.loseCount }}</span> Computer
+    </section>
+
+</template>
+
+<script>
+    export default{
+        props: ['winCount', 'loseCount']
+    }
+</script>
+
+<style lang="scss">
+
+    section.score {
+        border-bottom: 5px solid rgb(0, 0, 0);
+        padding: 24px;
+        font-size: 18px;
+    }
+
+    span {
+    padding: 10px;
+    font-weight: bold;
+    border: 2px solid black;
+    }
+
+</style>
